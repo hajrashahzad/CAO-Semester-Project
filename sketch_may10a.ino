1a50f6351b434e1d0223d677410386d2cf30db2a
Hajra <hshahzad.bscs20seecs@seecs.edu.pk>
2022-05-10 23:35:04 +0500

    Intital test code for blinking LED

---
+++ sketch_may10a.ino
@@ -0,0 +1,13 @@
+const int ledPin = 23;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                  // wait for a second
+  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                  // wait for a second
+}
